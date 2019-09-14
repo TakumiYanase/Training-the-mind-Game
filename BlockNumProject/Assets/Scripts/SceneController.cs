8402e483b98aea45c1ccc221c6a8ab4783f92f97
T.Yanase <TakumiYanase@outlook.jp>
2019-09-15 06:31:07 +0900

    ステージ選択時の関数を修正

--- BlockNumProject/Assets/Scripts/SceneController.cs
+++ BlockNumProject/Assets/Scripts/SceneController.cs
@@ -17,94 +17,10 @@ public class SceneController : MonoBehaviour
         SceneManager.LoadScene("StageSelectScene");
     }
 
-    // ステージ１に遷移
-    public void NextStage1()
+    // 任意ステージに遷移
+    public void ChoiceStage(int num)
     {
-        SceneManager.LoadScene("Stage1Scene");
-    }
-
-    // ステージ２に遷移
-    public void NextStage2()
-    {
-        SceneManager.LoadScene("Stage2Scene");
-    }
-
-    // ステージ３に遷移
-    public void NextStage3()
-    {
-        SceneManager.LoadScene("Stage3Scene");
-    }
-
-    // ステージ４に遷移
-    public void NextStage4()
-    {
-        SceneManager.LoadScene("Stage4Scene");
-    }
-
-    // ステージ５に遷移
-    public void NextStage5()
-    {
-        SceneManager.LoadScene("Stage5Scene");
-    }
-
-    // ステージ６に遷移
-    public void NextStage6()
-    {
-        SceneManager.LoadScene("Stage6Scene");
-    }
-
-    // ステージ７に遷移
-    public void NextStage7()
-    {
-        SceneManager.LoadScene("Stage7Scene");
-    }
-
-    // ステージ８に遷移
-    public void NextStage8()
-    {
-        SceneManager.LoadScene("Stage8Scene");
-    }
-
-    // ステージ９に遷移
-    public void NextStage9()
-    {
-        SceneManager.LoadScene("Stage9Scene");
-    }
-
-    // ステージ１０に遷移
-    public void NextStage10()
-    {
-        SceneManager.LoadScene("Stage10Scene");
-    }
-
-    // ステージ１１に遷移
-    public void NextStage11()
-    {
-        SceneManager.LoadScene("Stage11Scene");
-    }
-
-    // ステージ１２に遷移
-    public void NextStage12()
-    {
-        SceneManager.LoadScene("Stage12Scene");
-    }
-
-    // ステージ１３に遷移
-    public void NextStage13()
-    {
-        SceneManager.LoadScene("Stage13Scene");
-    }
-
-    // ステージ１４に遷移
-    public void NextStage14()
-    {
-        SceneManager.LoadScene("Stage14Scene");
-    }
-
-    // ステージ１５に遷移
-    public void NextStage15()
-    {
-        SceneManager.LoadScene("Stage15Scene");
+        SceneManager.LoadScene("Stage" + (num) + "Scene");
     }
 
     public void ExitGame()
