a9099a727aec2814b61137a4ae22a7fb6b88f956
T.Yanase <TakumiYanase@outlook.com>
2019-12-20 02:32:59 +0900

    CountDown 完成

---
+++ BlockNumProject/Assets/Scripts/CountDown.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class CountDown : MonoBehaviour
+{
+    [Serializable]
+    public struct RankingData
+    {
+        public int first;
+        public int second;
+        public int third;
+    }
+
+    [SerializeField]
+    private Text m_countDownText;
+
+    [SerializeField, Range(5.0f, 500.0f), HeaderAttribute("Limited Time")]
+    private float m_countTime = 300.0f;
+
+    private int m_seconds = 9999;
+
+    private int m_score = 0;
+
+    private int[] m_ranking = new int[3];
+
+    // 保存するファイル
+    const string SAVE_FILE_PATH = "Ranking.txt";
+
+    void Start()
+    {
+        m_countDownText = m_countDownText.GetComponent<Text>();
+
+        // フォルダからロード
+        var info = new FileInfo(Application.dataPath + "/" + SAVE_FILE_PATH);
+        var reader = new StreamReader(info.OpenRead());
+        var json = reader.ReadToEnd();
+        var data = JsonUtility.FromJson<RankingData>(json);
+
+        m_ranking[0] = data.first;
+        m_ranking[1] = data.second;
+        m_ranking[2] = data.third;
+    }
+
+    void Update()
+    {
+        GameObject.Find("CountDown(Clone)").GetComponent<Canvas>().worldCamera = Camera.main;
+
+        // カウントダウン
+        if (m_countTime > 0)
+        {
+            m_countTime -= Time.deltaTime;
+            m_seconds = (int)m_countTime;
+            m_countDownText.text = m_seconds.ToString();
+        }
+
+        if (m_seconds <= 0)
+        {
+            RankingCheck(m_ranking[0], m_ranking[1], m_ranking[2], m_score);
+        }
+    }
+
+    public void RankingCheck(int rankingFirst, int rankingSecond, int rankingThird, int score)
+    {
+        var data = new RankingData();
+
+        if (rankingFirst <= score)
+        {
+            data.third = data.second;
+            data.second = data.first;
+            data.first = score;
+        }
+        if (rankingSecond <= score && rankingFirst > score)
+        {
+            data.third = data.second;
+            data.second = score;
+        }
+        if (rankingThird <= score && rankingSecond > score)
+        {
+            data.third = score;
+        }
+
+        // JSONにシリアライズ
+        var json = JsonUtility.ToJson(data);
+        // Assetsフォルダに保存する
+        var path = Application.dataPath + "/" + SAVE_FILE_PATH;
+        var writer = new StreamWriter(path, false);
+        writer.WriteLine(json);
+        writer.Flush();
+        writer.Close();
+    }
+
+    public void AddScore()
+    {
+        m_score++;
+    }
+}
--- BlockNumProject/Assets/Scripts/GameController.cs
+++ BlockNumProject/Assets/Scripts/GameController.cs
@@ -9,176 +9,82 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     [SerializeField, HeaderAttribute("InputField")]
-    private InputField _inputField;
+    private InputField m_inputField;
 
     [SerializeField, HeaderAttribute("Text Data"), Space(5)]
-    private Text _text;
+    private Text m_text;
 
     [SerializeField]
-    private Text _countDownText;
-
-    [SerializeField]
-    private Text _answerText;
+    private Text m_answerText;
 
     [SerializeField,HeaderAttribute("Image Data"), Space(5)]
-    private Image _correctAnswer;
+    private Image m_correctAnswer;
     [SerializeField]
-    private Image _incorrectAnswer;
-    [SerializeField]
-    private Image _gameOver;
-
-    [SerializeField]
-    private Image _rankS;
-    [SerializeField]
-    private Image _rankA;
-    [SerializeField]
-    private Image _rankB;
-
-    [SerializeField, HeaderAttribute("Button Data"), Space(5)]
-    private Button _retry;
-    [SerializeField]
-    private Button _stageSelect;
-    [SerializeField]
-    private Button _nextStage;
+    private Image m_incorrectAnswer;
 
     [SerializeField,HeaderAttribute("SE Data"), Space(5)]
-    private AudioClip _correctSE;
+    private AudioClip m_correctSE;
     [SerializeField]
-    private AudioClip _incorrectSE;
-    [SerializeField]
-    private AudioClip _gameOverSE;
+    private AudioClip m_incorrectSE;
 
-    private AudioSource _audioSource;
+    private AudioSource m_audioSource;
 
-    [SerializeField,Range(1,25),HeaderAttribute("Stage Number"), TooltipAttribute("ステージのナンバーを入力する"),Space(5)]
-    private int _stageNum;
+    private bool m_correctFlag = false;
+    private bool m_incorrectFlag = false;
 
-    [SerializeField, Range(3.0f, 99.0f), HeaderAttribute("Limited Time"), TooltipAttribute("ステージの制限時間を入力する"), Space(5)]
-    private float _countTime = 5.0f;
+    [SerializeField, HeaderAttribute("SceneController Prefab"), Space(5)]
+    private SceneController m_sceneController = null;
 
-    private bool _correctFlag = false;
-    private bool _incorrectFlag = false;
-    private bool _gameOverFlag = false;
-
-    private int _seconds = 99;
+    private CountDown m_countDown = null;
 
     void Start()
     {
-        // GetComponent.
-        _inputField = _inputField.GetComponent<InputField>();
-        _text = _text.GetComponent<Text>();
-        _answerText = _answerText.GetComponent<Text>();
-        _countDownText = _countDownText.GetComponent<Text>();
-        _audioSource = GetComponent<AudioSource>();
+        m_inputField = m_inputField.GetComponent<InputField>();
+        m_text = m_text.GetComponent<Text>();
+        m_answerText = m_answerText.GetComponent<Text>();
+        m_audioSource = GetComponent<AudioSource>();
+        m_countDown = GameObject.Find("CountDown(Clone)").GetComponent<CountDown>();
 
-        _correctAnswer.gameObject.SetActive(false);
-        _incorrectAnswer.gameObject.SetActive(false);
-        _retry.gameObject.SetActive(false);
-        _stageSelect.gameObject.SetActive(false);
-        _nextStage.gameObject.SetActive(false);
-        _gameOver.gameObject.SetActive(false);
-        _rankS.gameObject.SetActive(false);
-        _rankA.gameObject.SetActive(false);
-        _rankB.gameObject.SetActive(false);
+        m_correctAnswer.gameObject.SetActive(false);
+        m_incorrectAnswer.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        // Count Down.
-        {
-            if (_countTime > 0)
-            {
-                _countTime -= Time.deltaTime;
+        if (m_correctFlag)
+            StartCoroutine(this.DelayMethod(1.0f, NextStage));
 
-                if (!_correctFlag && !_incorrectFlag && !_gameOverFlag)
-                {
-                    _seconds = (int)_countTime;
-                }
-
-                _countDownText.text = _seconds.ToString();
-            }
-        }
-            // Correct answer.
-            {
-                if (_correctFlag)
-            {
-                _stageSelect.gameObject.SetActive(true);
-
-                // Check time and display score rank.
-                CheckTime(_seconds);
-
-                if (_stageNum != 15)
-                    _nextStage.gameObject.SetActive(true);
-            }
-        }
-
-        // Incorrect answer.
-        if (_incorrectFlag)
-        {
-            _retry.gameObject.SetActive(true);
-            _stageSelect.gameObject.SetActive(true);
-        }
-
-        if (_seconds <= 0)
-            _gameOverFlag = true;
-
-        // Game over.
-        if (_gameOverFlag && !_gameOver.gameObject.activeInHierarchy)
-        {
-            _audioSource.PlayOneShot(_gameOverSE);
-            _gameOver.gameObject.SetActive(true);
-            _retry.gameObject.SetActive(true);
-            _stageSelect.gameObject.SetActive(true);
-        }
+        if(m_incorrectFlag)
+            StartCoroutine(this.DelayMethod(1.0f, NextStage));
     }
 
     public void InputText()
     {
-        // _inputField reflect.
-        _text.text = _inputField.text;
+        m_text.text = m_inputField.text;
     }
 
     public void CheckAnswer()
     {
-        // Judge an input value.
-        if (_inputField.text == _answerText.text)
+        // 判定
+        if (m_inputField.text == m_answerText.text)
         {
-            _correctAnswer.gameObject.SetActive(true);
-            _audioSource.PlayOneShot(_correctSE);
-            _correctFlag = true;
+            m_correctAnswer.gameObject.SetActive(true);
+            m_audioSource.PlayOneShot(m_correctSE);
+            m_correctFlag = true;
+            m_countDown.AddScore();
         }
         else
         {
-            _incorrectAnswer.gameObject.SetActive(true);
-            _audioSource.PlayOneShot(_incorrectSE);
-            _incorrectFlag = true;
+            m_incorrectAnswer.gameObject.SetActive(true);
+            m_audioSource.PlayOneShot(m_incorrectSE);
+            m_incorrectFlag = true;
         }
-    }
-
-    public void SelectRetry()
-    {
-        SceneManager.LoadScene("Stage" + _stageNum + "Scene");
-    }
-
-    public void ReturnStageSelect()
-    {
-        SceneManager.LoadScene("StageSelectScene");
     }
 
     public void NextStage()
     {
-        SceneManager.LoadScene("Stage" + (_stageNum + 1) + "Scene");
-    }
-
-    public void CheckTime(float seconds)
-    {
-        if (seconds >= 41 && seconds <= 60)
-            _rankS.gameObject.SetActive(true);
-
-        if (seconds >= 21 && seconds <= 40)
-            _rankA.gameObject.SetActive(true);
-
-        if (seconds >= 1 && seconds <= 20)
-            _rankB.gameObject.SetActive(true);
+        // ランダムにステージ遷移
+        SceneController sceneContoroller = m_sceneController.GetComponent<SceneController>();
+        sceneContoroller.RandomStage();
     }
 }
